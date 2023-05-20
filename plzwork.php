99804a19d339583351f9584b81087b62a617bef6
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-20 01:59:08 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -5,12 +5,12 @@ $email = $_POST['email'];
 $phoneNum = $_POST['phoneNum'];
 $contactReason = $_POST['contactReason'];
 
-$connect = new mysqli('192.168.18.5', 'SA', 'HMCap23!', 'CustomerInfo');
+$connect = new mysqli('192.168.18.5', 'SA', 'HMCap23!');
 if ($connect->connect_error){
     die('Connection Failed: '.$connect->connect_error);
 } else{
-    $stmt = $connect->prepare("Inert into Customers(fname, lname, email, phone, contactReason) values (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $fname, $lname, $email, $phoneNum, $contactReason);
+    //$stmt = $connect->prepare("Inert into Customers(fname, lname, email, phone, contactReason) values (?, ?, ?, ?, ?)");
+    //$stmt->bind_param("sssss", $fname, $lname, $email, $phoneNum, $contactReason);
     $stmt->execute();
     echo "Maybe worked";
     $stmt->close();
