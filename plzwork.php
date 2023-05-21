117320d69660ab8abf54bf004c15e597262c43c3
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-21 05:05:21 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -1,20 +1,35 @@
 <?php
 
 
-    $conn = mysqli_connect('192.168.18.5', 'root', '', 'CustomerInfo');
-    if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $email = $_POST['email'];
-        $phoneNum = $_POST['phoneNum'];
-        $contactReason = $_POST['contactReason'];
+    // $conn = mysqli_connect('192.168.18.5', 'root', '', 'CustomerInfo');
+    // if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
+    //     $fname = $_POST['fname'];
+    //     $lname = $_POST['lname'];
+    //     $email = $_POST['email'];
+    //     $phoneNum = $_POST['phoneNum'];
+    //     $contactReason = $_POST['contactReason'];
 
-        $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
-        $query = mysqli_query($conn,$sql);
-        if($query){
-            echo 'Entry Success';
-        }else{
-            echo 'Error';
-        }
-    }
+    //     $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
+    //     $query = mysqli_query($conn,$sql);
+    //     if($query){
+    //         echo 'Entry Success';
+    //     }else{
+    //         echo 'Error';
+    //     }
+    // }
+
+$host = '192.168.18.5';
+$usr = 'root';
+$pass = '';
+$db = 'CustomerInfo';
+
+$conn = mysqli_connect(hostname: $host,
+                       username: $usr,
+                       password: $pass,
+                       database: $db);
+
+if (mysqli_connect_errno()){
+    die("Connection Error: " . mysqli_connect_errno());
+}
+echo "Connection worked";
 ?>
