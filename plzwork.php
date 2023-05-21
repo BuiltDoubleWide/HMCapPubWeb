773e3e011c3db7bacb5d85970736689d0ba13284
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-21 01:19:35 -0600

    testing

--- plzwork.php
+++ plzwork.php
@@ -1,21 +1,23 @@
 <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
 
 
-$connect = mysqli_connect("192.168.18.5", 'root', '', 'CustomerInfo');
-if ($connect.mysqli_connect_error()){
-    die('Connection Failed: '.$connect.mysqli_connect_error());
-} else{
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $phoneNum = $_POST['phoneNum'];
-    $contactReason = $_POST['contactReason'];
+        $conn = mysqli_connect('192.168.18.5:3306', 'root', '', 'CustomerInfo') or die("Connection Failed: " .mysqli_connect_error());
+        echo ($conn.mysqli_connect_error());
+        if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $email = $_POST['email'];
+            $phoneNum = $_POST['phoneNum'];
+            $contactReason = $_POST['contactReason'];
 
-    $stmt = $connect->prepare("Inert into Customers(fname, lname, email, phone, contactReason) values (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $fname, $lname, $email, $phoneNum, $contactReason);
-    $stmt->execute();
-    echo "Maybe worked";
-    $stmt->close();
-    $connect->close();
-}
+            $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
+            $query = mysqli_query($conn,$sql);
+            if($query){
+                echo 'Entry Success';
+            }else{
+                echo 'Error';
+            }
+        }
+    }
 ?>
