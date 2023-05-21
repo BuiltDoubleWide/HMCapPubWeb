4a49d6ee57b922ca72b5b00d42f7adc509c00568
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-21 04:46:36 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -1,22 +1,20 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
 
 
-        $conn = mysqli_connect('192.168.18.5', 'root', '', 'CustomerInfo') or die("Connection Failed: " .mysqli_connect_error());
-        if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $email = $_POST['email'];
-            $phoneNum = $_POST['phoneNum'];
-            $contactReason = $_POST['contactReason'];
+    $conn = mysqli_connect('192.168.18.5', 'root', '', 'CustomerInfo') or die("Connection Failed: " .mysqli_connect_error());
+    if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $phoneNum = $_POST['phoneNum'];
+        $contactReason = $_POST['contactReason'];
 
-            $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
-            $query = mysqli_query($conn,$sql);
-            if($query){
-                echo 'Entry Success';
-            }else{
-                echo 'Error';
-            }
+        $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
+        $query = mysqli_query($conn,$sql);
+        if($query){
+            echo 'Entry Success';
+        }else{
+            echo 'Error';
         }
     }
 ?>
