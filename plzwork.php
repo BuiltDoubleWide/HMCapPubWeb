eda1a8c39f2373e4b028177ace7acfa62bb85b96
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-20 23:44:47 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -1,6 +1,7 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
         $conn = mysqli_connect('192.168.18.5', 'SA', 'HMCap23!', 'CustomerInfo') or die("Connection Failed: " .mysqli_connect_error());
+        echo ($conn.mysqli_connect_error());
         if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
             $fname = $_POST['fname'];
             $lname = $_POST['lname'];
