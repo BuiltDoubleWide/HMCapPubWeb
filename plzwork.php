ca57f9da481888d990e8f4e659de9f00fdd71d8d
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-20 20:57:50 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -1,9 +1,5 @@
 <?php
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$phoneNum = $_POST['phoneNum'];
-$contactReason = $_POST['contactReason'];
+
 
 
 $host = "192.168.18.5";
@@ -15,6 +11,12 @@ $connect = new mysqli($host, $user, $pass, $db);
 if ($connect->connect_error){
     die('Connection Failed: '.$connect->connect_error);
 } else{
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $phoneNum = $_POST['phoneNum'];
+    $contactReason = $_POST['contactReason'];
+
     $stmt = $connect->prepare("Inert into Customers(fname, lname, email, phone, contactReason) values (?, ?, ?, ?, ?)");
     $stmt->bind_param("sssss", $fname, $lname, $email, $phoneNum, $contactReason);
     $stmt->execute();
