c1b14139277291b371386f908b581911ecca130e
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-21 01:02:40 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -1,26 +1,27 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
 
 
-        $conn = mysqli_connect('192.168.18.5:3306', 'root', '', 'CustomerInfo');
-        if(mysqli_connect_error()){
-            echo "Connection Failed: " .mysqli_connect_error();
-        }
-        //echo ($conn.mysqli_connect_error());
-        if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $email = $_POST['email'];
-            $phoneNum = $_POST['phoneNum'];
-            $contactReason = $_POST['contactReason'];
 
-            $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
-            $query = mysqli_query($conn,$sql);
-            if($query){
-                echo 'Entry Success';
-            }else{
-                echo 'Error';
-            }
-        }
-    }
+$host = "192.168.18.5";
+$user = "root";
+$pass = '';
+$db = 'CustomerInfo';
+
+$connect = new mysqli($host, $user, $pass, $db);
+if ($connect->connect_error){
+    die('Connection Failed: '.$connect->connect_error);
+} else{
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $phoneNum = $_POST['phoneNum'];
+    $contactReason = $_POST['contactReason'];
+
+    $stmt = $connect->prepare("Inert into Customers(fname, lname, email, phone, contactReason) values (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $fname, $lname, $email, $phoneNum, $contactReason);
+    $stmt->execute();
+    echo "Maybe worked";
+    $stmt->close();
+    $connect->close();
+}
 ?>
