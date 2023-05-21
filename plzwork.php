36db53d07f72d2c6ea636b4e274989e7dcdf81d0
BuiltDoubleWide <81836585+BuiltDoubleWide@users.noreply.github.com>
2023-05-20 23:59:51 -0600

    Update plzwork.php

--- plzwork.php
+++ plzwork.php
@@ -1,21 +1,27 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
-        $conn = mysqli_connect('192.168.18.5', 'SA', 'HMCap23!', 'CustomerInfo') or die("Connection Failed: " .mysqli_connect_error());
-        echo ($conn.mysqli_connect_error());
-        if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $email = $_POST['email'];
-            $phoneNum = $_POST['phoneNum'];
-            $contactReason = $_POST['contactReason'];
 
-            $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
-            $query = mysqli_query($conn,$sql);
-            if($query){
-                echo 'Entry Success';
-            }else{
-                echo 'Error';
-            }
+        $conn = mysqli_connect('192.168.18.5', 'SA', 'HMCap23!', 'CustomerInfo');
+        if(!$conn){
+            echo 'Connection error: ' . mysqli_connect_error();
         }
+
+        // $conn = mysqli_connect('192.168.18.5', 'SA', 'HMCap23!', 'CustomerInfo') or die("Connection Failed: " .mysqli_connect_error());
+        // echo ($conn.mysqli_connect_error());
+        // if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($__POST['phoneNum']) && isset($_POST['contactReason'])){
+        //     $fname = $_POST['fname'];
+        //     $lname = $_POST['lname'];
+        //     $email = $_POST['email'];
+        //     $phoneNum = $_POST['phoneNum'];
+        //     $contactReason = $_POST['contactReason'];
+
+        //     $sql = "INSERT INTO `Customers` (`fname`, `lname`, `email`, `phone`, `contactReason`) VALUES ('$fname', '$lname', '$email', '$phoneNum', '$contactReason')";
+        //     $query = mysqli_query($conn,$sql);
+        //     if($query){
+        //         echo 'Entry Success';
+        //     }else{
+        //         echo 'Error';
+        //     }
+        // }
     }
 ?>
